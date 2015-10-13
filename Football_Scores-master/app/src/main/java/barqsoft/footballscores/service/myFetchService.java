9953d4de4dd823c017a71a63883f17bf14539995
bora <baybora.oren@ucuzcu.com>
2015-10-13 17:53:08 +0300

    api key implementation

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/service/myFetchService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/service/myFetchService.java
@@ -22,8 +22,10 @@ import java.util.Date;
 import java.util.TimeZone;
 import java.util.Vector;
 
+import barqsoft.footballscores.BuildConfig;
 import barqsoft.footballscores.DatabaseContract;
 import barqsoft.footballscores.R;
+
 
 /**
  * Created by yehya khaled on 3/2/2015.
@@ -63,7 +65,7 @@ public class myFetchService extends IntentService
             URL fetch = new URL(fetch_build.toString());
             m_connection = (HttpURLConnection) fetch.openConnection();
             m_connection.setRequestMethod("GET");
-            m_connection.addRequestProperty("X-Auth-Token",getString(R.string.api_key));
+            m_connection.addRequestProperty("X-Auth-Token", BuildConfig.MOVIE_API_KEY);
             m_connection.connect();
 
             // Read the input stream into a String
